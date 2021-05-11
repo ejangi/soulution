d11da2518538c49f501f5364630dea983d56fdea
ejangi <james@ejangi.com>
2021-05-11 19:15:50 +1000

    Pass substitution variables through to the bash script that replaces them in the firebase.config.js file.

--- substitutevars.sh
+++ substitutevars.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-sed -i "s/process.env._API_KEY/$_API_KEY/g" src/src/firebase.config.js
-sed -i "s/process.env._AUTH_DOMAIN/$_AUTH_DOMAIN/g" src/src/firebase.config.js
-sed -i "s/process.env.PROJECT_ID/$PROJECT_ID/g" src/src/firebase.config.js
-sed -i "s/process.env._STORAGE_BUCKET/$_STORAGE_BUCKET/g" src/src/firebase.config.js
-sed -i "s/process.env._MESSAGE_SENDER_ID/$_MESSAGE_SENDER_ID/g" src/src/firebase.config.js
-sed -i "s/process.env._APP_ID/$_APP_ID/g" src/src/firebase.config.js
+sed -i "s/process.env._API_KEY/\"$_API_KEY\"/g" src/src/firebase.config.js
+sed -i "s/process.env._AUTH_DOMAIN/\"$_AUTH_DOMAIN\"/g" src/src/firebase.config.js
+sed -i "s/process.env.PROJECT_ID/\"$PROJECT_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env._STORAGE_BUCKET/\"$_STORAGE_BUCKET\"/g" src/src/firebase.config.js
+sed -i "s/process.env._MESSAGE_SENDER_ID/\"$_MESSAGE_SENDER_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env._APP_ID/\"$_APP_ID\"/g" src/src/firebase.config.js
+
+cat src/src/firebase.config.js
