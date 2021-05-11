bf02561958fa0fa08c840a2b1d93d709ee06d1e1
ejangi <james@ejangi.com>
2021-05-11 20:02:37 +1000

    Fixed an issue with environment variables not working in development

--- src/src/firebase.config.js
+++ src/src/firebase.config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 let config = {
-    apiKey: process.env._API_KEY,
-    authDomain: process.env._AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env._STORAGE_BUCKET,
-    messagingSenderId: process.env._MESSAGE_SENDER_ID,
-    appId: process.env._APP_ID
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(config);
--- substitutevars.sh
+++ substitutevars.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sed -i "s/process.env._API_KEY/\"$_API_KEY\"/g" src/src/firebase.config.js
-sed -i "s/process.env._AUTH_DOMAIN/\"$_AUTH_DOMAIN\"/g" src/src/firebase.config.js
-sed -i "s/process.env.PROJECT_ID/\"$PROJECT_ID\"/g" src/src/firebase.config.js
-sed -i "s/process.env._STORAGE_BUCKET/\"$_STORAGE_BUCKET\"/g" src/src/firebase.config.js
-sed -i "s/process.env._MESSAGE_SENDER_ID/\"$_MESSAGE_SENDER_ID\"/g" src/src/firebase.config.js
-sed -i "s/process.env._APP_ID/\"$_APP_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_API_KEY/\"$REACT_API_KEY\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_AUTH_DOMAIN/\"$REACT_AUTH_DOMAIN\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_PROJECT_ID/\"$REACT_PROJECT_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_STORAGE_BUCKET/\"$REACT_STORAGE_BUCKET\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_MESSAGE_SENDER_ID/\"$REACT_MESSAGE_SENDER_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_ID/\"$REACT_APP_ID\"/g" src/src/firebase.config.js
 
 cat src/src/firebase.config.js
