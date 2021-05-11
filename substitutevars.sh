2654cc15f75ed6d61ce5f5f4b2bae22a41ad6c1e
ejangi <james@ejangi.com>
2021-05-11 20:07:15 +1000

    Fix issue with the substitution variables.

--- substitutevars.sh
+++ substitutevars.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sed -i "s/process.env.REACT_API_KEY/\"$REACT_API_KEY\"/g" src/src/firebase.config.js
-sed -i "s/process.env.REACT_AUTH_DOMAIN/\"$REACT_AUTH_DOMAIN\"/g" src/src/firebase.config.js
-sed -i "s/process.env.REACT_PROJECT_ID/\"$REACT_PROJECT_ID\"/g" src/src/firebase.config.js
-sed -i "s/process.env.REACT_STORAGE_BUCKET/\"$REACT_STORAGE_BUCKET\"/g" src/src/firebase.config.js
-sed -i "s/process.env.REACT_MESSAGE_SENDER_ID/\"$REACT_MESSAGE_SENDER_ID\"/g" src/src/firebase.config.js
-sed -i "s/process.env.REACT_APP_ID/\"$REACT_APP_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_API_KEY/\"$REACT_APP_API_KEY\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_AUTH_DOMAIN/\"$REACT_APP_AUTH_DOMAIN\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_PROJECT_ID/\"$REACT_APP_PROJECT_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_STORAGE_BUCKET/\"$REACT_APP_STORAGE_BUCKET\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_MESSAGE_SENDER_ID/\"$REACT_APP_MESSAGE_SENDER_ID\"/g" src/src/firebase.config.js
+sed -i "s/process.env.REACT_APP_APP_ID/\"$REACT_APP_APP_ID\"/g" src/src/firebase.config.js
 
 cat src/src/firebase.config.js
