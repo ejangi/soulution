1eeffa025cd0cdc81cc26ad4b5747526593946f5
ejangi <james@ejangi.com>
2021-05-15 07:35:01 +1000

    Use internal state for problemsList

--- src/src/components/problemsList.js
+++ src/src/components/problemsList.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import timeAgo from '../helpers/timeAgo';
 
 function ProblemsList(props) {
+    const [problems, setProblems] = useState(props.problems);
+
+    useEffect(() => {
+        setProblems(props.problems);
+    }, [props]);
+
     const handleClick = (e, id) => {
        props.getProblem(id);
     };
@@ -9,7 +15,7 @@ function ProblemsList(props) {
     return(
         <div className="problems-list">
         {
-            props.problems && props.problems.map((problem) => {
+            problems && problems.map((problem) => {
                 return(
                     <div className="problem" id={problem.id} key={problem.id} onClick={(e) => handleClick(e, problem.id)}>
                         <h3>{problem.Title}</h3>
