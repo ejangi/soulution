39c34984776122a0bc9e258a788d87ebcf322beb
James Angus <james@ejangi.com>
2025-12-07 10:40:59 +1000

    Build fixes

--- src/src/components/FirebaseAuthUI.js
+++ src/src/components/FirebaseAuthUI.js
@@ -1,15 +1,16 @@
-import React, { useEffect, useRef } from 'react';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import * as firebaseui from 'firebaseui';
-import 'firebaseui/dist/firebaseui.css';
+import React, { useEffect, useRef } from "react";
+import "firebase/compat/auth";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
 
 function FirebaseAuthUI({ uiConfig, firebaseAuth }) {
   const uiRef = useRef(null);
 
   useEffect(() => {
     // Initialize FirebaseUI
-    const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebaseAuth);
+    const ui =
+      firebaseui.auth.AuthUI.getInstance() ||
+      new firebaseui.auth.AuthUI(firebaseAuth);
 
     // Start the sign-in flow
     ui.start(uiRef.current, uiConfig);
