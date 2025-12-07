d00ca99f634a986b56b8f91208932e701703600b
James Angus <james@ejangi.com>
2025-12-07 10:07:23 +1000

    Fixed issues related to the update to packages

--- src/src/index.js
+++ src/src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+// Import createRoot from the client entry point
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.querySelector('body')
-);
+// 1. Find the container element.
+// It's more standard to use document.getElementById('root')
+// instead of document.querySelector('body') for the root.
+// We'll use document.getElementById('root') for best practice,
+// but ensure your public/index.html has a matching element.
+const container = document.getElementById("root");
+
+// If the container is not found, you can add an error check:
+if (!container) throw new Error("Failed to find the root element!");
+
+// 2. Create a root.
+const root = createRoot(container);
+
+// 3. Render the application using the new root API.
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
